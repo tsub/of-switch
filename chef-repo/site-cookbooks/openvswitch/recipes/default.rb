c8e2686a79a15849196c24029ab73de2a7f74551
tsubasa <b1207000@planet.kanazawa-it.ac.jp>
2015-10-27 18:48:49 +0900

    add openvswitch vagrant and chef cookbook

---
+++ chef-repo/site-cookbooks/openvswitch/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: openvswitch
+# Recipe:: default
+#
+# Copyright 2015, Tsubasa Takayama
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'openvswitch-switch' do
+  action :install
+end
