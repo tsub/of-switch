1d4fbe099b7692e757536e3bf91e8e3d3030a844
tsubasa <b1207000@planet.kanazawa-it.ac.jp>
2015-11-04 16:41:35 +0900

    add ovs bridge

--- chef-repo/site-cookbooks/openvswitch/recipes/default.rb
+++ chef-repo/site-cookbooks/openvswitch/recipes/default.rb
@@ -15,3 +15,11 @@ service 'openvswitch-switch' do
   action [:start, :enable]
   supports start: true, status: true, restart: true, reload: true
 end
+
+template 'br1' do
+  path '/etc/network/interfaces.d/br1'
+  source 'br1.erb'
+  mode 0644
+  owner 'root'
+  group 'root'
+end
